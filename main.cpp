bcd27b081537ba7fafcaff1f26fbc0a4f85fca43
Brendan Charen <jedi2funny@gmail.com>
2026-04-09 12:16:57 -0400

    fixed door & key drawing

--- main.cpp
+++ main.cpp
@@ -14,11 +14,17 @@ level level1 = {{Rectangle{450,800,100,50} , Rectangle{600,1200,300,50}},{},{}};
 void updateEnvironment(){
     for (int i = 0; i < level1.walls.size(); i++){
         DrawRectangleRec(level1.walls[i],{42,2,57,255});
-        DrawRectangleRec(level1.keys[i],{242,132,17,255});
-        DrawRectangleRec(level1.doors[i],{72,128,7,255});
-
         zippy.collisionCheck(level1.walls[i]);
     }
+    for (int i = 0; i < level1.walls.size(); i++){
+        DrawRectangleRec(level1.keys[i],{242,132,17,255});
+        //zippy.collisionCheck(level1.keys[i]);
+    }
+    for (int i = 0; i < level1.walls.size(); i++){
+        DrawRectangleRec(level1.doors[i],{72,128,7,255});
+        //zippy.collisionCheck(level1.doors[i]);
+    }
+        
 } 
 
 int main () {
